f745f886061c9bdb27f02d138221ed4f1e2c4061
Ajay12338 <ajaysam12338@gmail.com>
2023-12-09 13:20:33 +0530

    Added clipboard copy featuregit add .

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ export const header = document.querySelector("header");
 const colorPicker = document.getElementById("color-picker");
 const colorLists = document.getElementById("color-lists");
 const darkModeCheckbox = document.getElementById("toggleBtn");
-
 const baseURL = " https://www.thecolorapi.com/scheme";
 
+if (darkModeCheckbox.checked) darkMode();
 darkModeCheckbox.addEventListener("click", () => {
   darkModeCheckbox.checked ? darkMode() : lightMode();
 });
@@ -27,14 +27,16 @@ const displayFooter = (hexValues) => {
   }
   footer.innerHTML = footerHtml;
 };
-
+const copyToClipBoard = async (color) => {
+  await navigator.clipboard.writeText(color);
+  alert(`Copied ${color} to clipboard!!!`);
+};
 const generateColors = (colors) => {
   let htmlColors = "";
   let hexColors = [];
   for (const color of colors) {
     htmlColors += `
-        <div style ="background:${color.hex.value}">
-           
+        <div style ="background:${color.hex.value}"> 
         </div>
         `;
     hexColors.push(color.hex.value);
@@ -48,5 +50,13 @@ getColorBtn.addEventListener("click", () => {
   }&count=10`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => generateColors(data.colors));
+    .then((data) => {
+      generateColors(data.colors);
+      const footerTxts = document.querySelectorAll("footer > div");
+      footerTxts.forEach((div) => {
+        div.addEventListener("click", (e) => {
+          copyToClipBoard(div.innerText);
+        });
+      });
+    });
 });
